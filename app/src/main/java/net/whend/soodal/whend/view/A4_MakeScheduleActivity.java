ad71ba23e6c399a0e58d8d989940700254e10a24
JuneB27 <hanabira207@gmail.com>
2015-07-19 17:55:07 +0900

    이미지업로드

--- app/src/main/java/net/whend/soodal/whend/view/A4_MakeScheduleActivity.java
+++ app/src/main/java/net/whend/soodal/whend/view/A4_MakeScheduleActivity.java
@@ -1,15 +1,26 @@
 package net.whend.soodal.whend.view;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +30,16 @@ import net.whend.soodal.whend.R;
 
 public class A4_MakeScheduleActivity extends AppCompatActivity {
 
+    private int TAKE_FROM_CAMERA = 1;
+    private int TAKE_FROM_GALLERY = 2;
+
+    private int REQ_CODE_SELECT_IMAGE = 100;
+    private Uri mImageCaptureUri;
+    private String selectedImagePath;
+
+    private ImageView schedule_photo;
+    private ImageView schedule_photo_add;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +48,18 @@ public class A4_MakeScheduleActivity extends AppCompatActivity {
 
         String sDate, sContent, sLocation, sTime;
 
+        View.OnClickListener photo_add;
+
         EditText date = (EditText) findViewById(R.id.date);
         EditText time = (EditText) findViewById(R.id.time);
         EditText title = (EditText) findViewById(R.id.title);
         EditText location = (EditText) findViewById(R.id.location);
+        EditText memo = (EditText) findViewById(R.id.memo);
 
-        Intent intent=new Intent(this.getIntent());
+        schedule_photo = (ImageView) findViewById(R.id.schedule_photo);
+        schedule_photo_add = (ImageView) findViewById(R.id.schedule_photo_add);
+
+        Intent intent = new Intent(this.getIntent());
         sDate = intent.getStringExtra("date");
         sContent = intent.getStringExtra("content");
         sLocation = intent.getStringExtra("location");
@@ -64,7 +91,117 @@ public class A4_MakeScheduleActivity extends AppCompatActivity {
         });
 
 
+        // 클릭리스너
+
+        photo_add = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final CharSequence[] items = {"카메라에서 찍기", "갤러리에서 가져오기"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(A4_MakeScheduleActivity.this, R.style.AppCompatAlertDialogStyle);
+                        // 각 항목을 설정하고 클릭했을 때 동작을 지정함
+
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (item == 0) { //카메라에서 찍기
+
+                            // 카메라 호출
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
+
+                            // 이미지 잘라내기 위한 크기
+
+                            float width_dpi = getResources().getDisplayMetrics().densityDpi;
+                            float height_dpi = 120f;
+
+                            float ratio = width_dpi/height_dpi;
+
+                            intent.putExtra("crop", "true");
+                            intent.putExtra("aspectX", 0);
+                            intent.putExtra("aspectY", 0);
+                            intent.putExtra("outputX", 200);
+                            intent.putExtra("outputY", 150);
+
+                            try {
+                                intent.putExtra("return-data", true);
+                                startActivityForResult(Intent.createChooser(intent,
+                                        "Complete action using"), TAKE_FROM_CAMERA);
+                            } catch (ActivityNotFoundException e) {
+                                // Do nothing for now
+                            }
+
+                        } else if (item == 1) { //갤러리에서 가져오기
+
+
+                            Intent intent = new Intent();
+                            // Gallery 호출
+                            intent.setType("image/*");
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            // 잘라내기 셋팅
+
+                            float width_dp = (float) getResources().getDisplayMetrics().densityDpi;
+                            float height_dp = 120f;
+
+                            System.out.println("너비 : " + width_dp);
+                            System.out.println("높이 : " + height_dp);
+
+                            float ratio = width_dp/height_dp;
+
+                            System.out.println("비율 : " + ratio);
+
+
+                            intent.putExtra("crop", "true");
+                            intent.putExtra("aspectX", 1);
+                            intent.putExtra("aspectY", ratio);
+                            intent.putExtra("outputX", 200);
+                            intent.putExtra("outputY", 150);
+                            try {
+                                intent.putExtra("return-data", true);
+                                startActivityForResult(Intent.createChooser(intent,
+                                        "Complete action using"), TAKE_FROM_GALLERY);
+                            } catch (ActivityNotFoundException e) {
+                                // Do nothing for now
+                            }
+                        }
+
+                    }
+                });
+
+                builder.show();
+
+            }
+        };
+
+        schedule_photo.setOnClickListener(photo_add);
+        schedule_photo_add.setOnClickListener(photo_add);
+
     }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == TAKE_FROM_GALLERY) {
+                Bundle extras = data.getExtras();
+                mImageCaptureUri = data.getData(); // Get data from selected photo
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    System.out.println(photo==null? "null":"nonull");
+                    schedule_photo.setImageBitmap(photo);
+                }
+            }else if (requestCode == TAKE_FROM_CAMERA) {
+                Bundle extras2 = data.getExtras();
+                if (extras2 != null) {
+                    Bitmap photo = extras2.getParcelable("data");
+                    schedule_photo.setImageBitmap(photo);
+                }
+            }
+        }
+    }
+
+
 
 
     @Override
