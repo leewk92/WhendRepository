014ac8af1b729e92c11398a54555de01231c5d76
JuneB27 <hanabira207@gmail.com>
2015-08-08 18:31:14 +0900

    페이스북 아이디가입(A0_3_SignUpFromFacebook) + 패스워드변경 레이아웃 (S4_Password)

---
+++ app/src/main/java/net/whend/soodal/whend/view/A0_3_SignUpFromFacebook.java
@@ -0,0 +1,40 @@
+package net.whend.soodal.whend.view;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.whend.soodal.whend.R;
+
+public class A0_3_SignUpFromFacebook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a0_3_sign_up_from_facebook);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_a0_3__sign_up_from_facebook, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
