8e3ae769d41d89b103432b1784084b51290f4455
JuneB27 <hanabira207@gmail.com>
2015-08-30 19:23:03 +0900

    1. (New Feature) # tag search

--- app/src/main/java/net/whend/soodal/whend/view/setting/S7_Test.java
+++ app/src/main/java/net/whend/soodal/whend/view/setting/S7_Test.java
@@ -1,42 +1,16 @@
 package net.whend.soodal.whend.view.setting;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Spannable;
 import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.TextUtils;
-import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import net.whend.soodal.whend.R;
-import net.whend.soodal.whend.util.HTTPRestfulUtilizer;
-import net.whend.soodal.whend.util.MultiAutoCompleteTextViewSharp;
-import net.whend.soodal.whend.util.SpannableStringMaker;
-import net.whend.soodal.whend.util.hashtagSpan;
-import net.whend.soodal.whend.view.A7_SpecificHashTagActivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import static net.whend.soodal.whend.util.SpannableStringMaker.*;
 
 public class S7_Test extends AppCompatActivity {
 
@@ -49,10 +23,10 @@ public class S7_Test extends AppCompatActivity {
         TextView tv =(TextView)findViewById(R.id.test_tv);
         EditText et = (EditText)findViewById(R.id.test_et);
 
-        MultiAutoCompleteTextViewSharp mactv = (MultiAutoCompleteTextViewSharp)findViewById(R.id.test_mtac);
+        MultiAutoCompleteTextView mactv = (MultiAutoCompleteTextView)findViewById(R.id.test_mtac);
         ArrayAdapter<String> aa2 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line,
-                new String[] {"연세대학교", "수달", "회의", "유포니아", "이원철하우스", "테스트", "테이프", "유글레나", "연세대" });
+                new String[] {"연세대학교", "수달", "회의", "유포니아", "이원철하우스", "테스트", "테이프", "유글레나", "연세대","blah","next","previous" });
 
         mactv.setAdapter(aa2);
 
@@ -89,10 +63,10 @@ public class S7_Test extends AppCompatActivity {
         public int findTokenStart(CharSequence text, int cursor) {
             int i = cursor;
 
-            while (i > 0 && text.charAt(i - 1) != ' ') {
+            while (i > 0 && text.charAt(i - 1) != '#') {
                 i--;
             }
-            while (i < cursor && text.charAt(i) == ' ') {
+            while (i < cursor && text.charAt(i) == '#') {
                 i++;
             }
 
@@ -104,7 +78,7 @@ public class S7_Test extends AppCompatActivity {
             int len = text.length();
 
             while (i < len) {
-                if (text.charAt(i) == ' ') {
+                if (text.charAt(i) == ' ' || text.charAt(i) == '#') {
                     return i;
                 } else {
                     i++;
@@ -117,16 +91,15 @@ public class S7_Test extends AppCompatActivity {
         public CharSequence terminateToken(CharSequence text) {
             int i = text.length();
 
-            while (i > 0 && text.charAt(i - 1) == ' ') {
+            while (i > 0 && (text.charAt(i - 1) == ' ' || text.charAt(i-1) == '#')) {
                 i--;
             }
 
-            if (i > 0 && text.charAt(i - 1) == ' ') {
+            if (i > 0 && (text.charAt(i - 1) == ' ' || text.charAt(i-1) == '#')) {
                 return text;
             } else {
                     SpannableString sp = new SpannableString("#"+text + " ");
-                    sp.setSpan(new ForegroundColorSpan(Color.parseColor("#0288D1")), 0, text.length()+2,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    //sp.setSpan(new ForegroundColorSpan(Color.parseColor("#0288D1")), 0, text.length()+2,Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
                     return sp;
             }
